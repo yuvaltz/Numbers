721d681f3ee42928372e74f94f4784a7d5fe851d
YuvalT <yuvaltze@gmail.com>
2014-10-04 21:50:47 +0300

    Controls - Link

---
+++ Numbers.Web/Controls/Link.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Html;
+
+namespace Numbers.Web.Controls
+{
+    public class Link : Control
+    {
+        public string Text
+        {
+            get { return linkElement.TextContent; }
+            set { linkElement.TextContent = value; }
+        }
+
+        public string Href
+        {
+            get { return linkElement.GetAttribute("href"); }
+            set { linkElement.SetAttribute("href", value); }
+        }
+
+        private Element linkElement;
+
+        public Link(string className = String.Empty) :
+            base("link", className)
+        {
+            linkElement = Document.CreateElement("a");
+            linkElement.SetAttribute("target", "_blank");
+
+            HtmlElement.AppendChild(linkElement);
+        }
+    }
+}
